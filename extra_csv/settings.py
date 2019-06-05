bc5ea304e96d8471f89f0c63f46321c8c4e74ce9
Gabriel Mourão <gabrielmouraodemelo@gmail.com>
2019-06-05 19:59:57 -0300

    Remoção de dados estáticos
    Remoção de dados estáticos errados no arquivo

--- extra_csv/settings.py
+++ extra_csv/settings.py
@@ -1,11 +1,3 @@
 # settings.py
 from dotenv import load_dotenv
 load_dotenv()
-
-# OR, the same with increased verbosity:
-load_dotenv(verbose=True)
-
-# OR, explicitly providing path to '.env'
-from pathlib import Path  # python3 only
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
